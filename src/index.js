14e3aac970d23a49204d25b8e9a5ece5ba8ac406
Tarley Campos <tarley.campos@localiza.com>
2019-01-05 00:29:11 -0200

    feat: Configura o redux e implementa as actios adicionar e remover receitas

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux'
+import reducer from './reducers'
+
+const store = createStore(
+	reducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+//console.log(store.getState());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
