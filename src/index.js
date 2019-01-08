5df184908f6c3fbf35baea67c81651708f403fb9
Tarley Campos <tarley.campos@localiza.com>
2019-01-08 19:59:54 -0200

    feat: Cria um middleware para log da aplicação

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import logger from 'redux-logger';
 
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import reducer from './reducers';
 
+/*
+const logger = store => next => action => {
+	console.group(action.type);
+	console.info('dispatching', action);
+	let result = next(action);
+	console.log('next state', store.getState());
+	console.groupEnd(action.type);
+	return result
+}
+*/
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 const store = createStore(
 	reducer,
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	composeEnhancers(applyMiddleware(logger))
 );
 
 //console.log(store.getState());
