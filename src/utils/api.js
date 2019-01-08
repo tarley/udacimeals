1294e7ffde8362599911146ab98095153b7725fb
Tarley Campos <tarley.campos@localiza.com>
2019-01-07 23:07:09 -0200

    feat: Permite adicionar receitas ao calendario e remover
    Também permite a visualização da lista de compras.

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,9 +2,14 @@ const API_ID = process.env.REACT_APP_API_ID;
 const APP_KEY = process.env.REACT_APP_APP_KEY;
 
 export function fetchRecipes(food = '') {
+	const headers = new Headers();
+	headers.append('Access-Control-Allow-Origin', '*')
+
+	const init = {headers};
+
 	food = food.trim();
 
-	return fetch(`https://api.edamam.com/search?q=${food}&app_id=${API_ID}&app_key=${APP_KEY}`)
+	return fetch(`https://api.edamam.com/search?q=${food}&app_id=${API_ID}&app_key=${APP_KEY}`, init)
 				.then(res => res.json())
 				.then(({hits}) => hits.map(({recipe}) => recipe));
 }
